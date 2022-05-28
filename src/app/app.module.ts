e46f69978ca2e576d0d9aca737a79c70be648ef6
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-28 23:27:53 +0530

    Generate directive named class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClassDirective } from './class.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassDirective
   ],
   imports: [
     BrowserModule,
