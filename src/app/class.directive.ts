a8c83afbf2efb1d75de77326b3848e79afd057d3
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-28 23:53:21 +0530

    Use set timeout to set bg color && Expect backgroundcolor from parent

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,12 +1,18 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
 
+  @Input() backgroundColor?: string;
+
   constructor(private element: ElementRef) { 
-    this.element.nativeElement.style.backgroundColor = "yellow";
+    setTimeout(() => {
+      this.element.nativeElement.style.backgroundColor = this.backgroundColor
+    }, 50
+    
+    );
   }
 
 }
