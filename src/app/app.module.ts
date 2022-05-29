84ed1dcfb9c1969cd79e78980138a2f42e489321
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-29 22:33:16 +0530

    Replace ngFor with appTimes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClassDirective } from './class.directive';
+import { TimesDirective } from './times.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClassDirective
+    ClassDirective,
+    TimesDirective
   ],
   imports: [
     BrowserModule,
