5bbf20cc6af42d8d22aeaaeaa62c915c2391a522
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-29 13:32:11 +0530

    Intercept anytime angular tries to set a property on our directive

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,18 +1,18 @@
 import { Directive, ElementRef, Input } from '@angular/core';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
 
-  @Input() backgroundColor?: string;
-
   constructor(private element: ElementRef) { 
-    setTimeout(() => {
-      this.element.nativeElement.style.backgroundColor = this.backgroundColor
-    }, 50
-    
-    );
+  }
+  // Intercept anytime angular tries to set a property on our directive
+  // As soon as we intercept that, we are gonna take the value that angular trying to set
+  // that property to and we are going to use it to actually update something else.
+  @Input() set backgroundColor(color: string) {
+    this.element.nativeElement.style.backgroundColor = color;
   }
 
 }
