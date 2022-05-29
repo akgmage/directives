0f454f113679cd1127c38261aa32e928e9be932e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-29 13:42:33 +0530

    Redirect appClass to backgroundColor method

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,5 +1,4 @@
 import { Directive, ElementRef, Input } from '@angular/core';
-import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 
 @Directive({
   selector: '[appClass]'
@@ -11,7 +10,9 @@ export class ClassDirective {
   // Intercept anytime angular tries to set a property on our directive
   // As soon as we intercept that, we are gonna take the value that angular trying to set
   // that property to and we are going to use it to actually update something else.
-  @Input() set appClass(color: string) {
+
+  // Whenever we try to set appClass property, Input decorator is gonna redirect to backgroundColor method
+  @Input('appClass') set backGroundColor(color: string) {
     this.element.nativeElement.style.backgroundColor = color;
   }
 
